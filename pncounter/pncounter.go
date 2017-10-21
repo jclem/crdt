ea548aca24bc19db1c76667e4534f58c3eb08753
Jonathan Clem <jonathan@jclem.net>
2017-10-21 12:30:28 -0400

    Add passing pncounter tests

--- pncounter/pncounter.go
+++ pncounter/pncounter.go
@@ -2,39 +2,47 @@ package pncounter
 
 type id string
 
-// A PNCounter is a counter that can both grow and shrink
+// A PNCounter is a counter that can both grow and shrink.
 type PNCounter struct {
 	id   id
 	vals map[id]*[2]int
 }
 
-// NewPNCounter creates a new PNCounter
+// NewPNCounter creates a new PNCounter.
 func NewPNCounter(gid id) *PNCounter {
-	return &PNCounter{id: gid, vals: make(map[id]*[2]int)}
+	return &PNCounter{
+		id:   gid,
+		vals: map[id]*[2]int{gid: &[2]int{}},
+	}
 }
 
-// Increment increments the value at this site for the PNCounter
+// Increment increments the value at this site for the PNCounter.
 func (p *PNCounter) Increment() {
-	p.vals[p.id][0] = 1
+	p.vals[p.id][0]++
 }
 
-// Decrement decrements the value at this site for the PNCounter
+// Decrement decrements the value at this site for the PNCounter.
 func (p *PNCounter) Decrement() {
 	p.vals[p.id][1]++
 }
 
-// Incorporate incorporates a remote GCounter value
-func (p *PNCounter) Incorporate(id id, vals [2]int) {
-	if vals[0] > p.vals[id][0] {
-		p.vals[id][0] = vals[0]
+// Incorporate incorporates a remote GCounter value.
+func (p *PNCounter) Incorporate(id id, siteVal [2]int) {
+	if _, ok := p.vals[id]; !ok {
+		p.vals[id] = &siteVal
+		return
 	}
 
-	if vals[1] > p.vals[id][1] {
-		p.vals[id][1] = vals[1]
+	if siteVal[0] > p.vals[id][0] {
+		p.vals[id][0] = siteVal[0]
+	}
+
+	if siteVal[1] > p.vals[id][1] {
+		p.vals[id][1] = siteVal[1]
 	}
 }
 
-// Value gets the value of the PNCounter
+// Value gets the value of the PNCounter.
 func (p *PNCounter) Value() int {
 	sum := 0
 	for _, val := range p.vals {
--- pncounter/pncounter_test.go
+++ pncounter/pncounter_test.go
@@ -1,1 +1,29 @@
 package pncounter_test
+
+import (
+	"testing"
+
+	"github.com/jclem/crdt/pncounter"
+)
+
+func TestIncrement(t *testing.T) {
+	g := pncounter.NewPNCounter("A")
+	g.Increment()
+	if v := g.Value(); v != 1 {
+		t.Fatalf("Expected 1, got %q", v)
+	}
+}
+
+func TestIncorporate(t *testing.T) {
+	g := pncounter.NewPNCounter("A")
+	g.Increment()
+	g.Increment()
+	g.Decrement()
+	g.Incorporate("B", [2]int{2, 3})
+	g.Incorporate("B", [2]int{1, 4})
+	g.Incorporate("C", [2]int{3, 0})
+
+	if exp, v := 2, g.Value(); v != 2 {
+		t.Fatalf("Expected %d, got %d", exp, v)
+	}
+}
